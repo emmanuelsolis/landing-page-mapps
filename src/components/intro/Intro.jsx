9ab7072e967cf9af74db431d418e9a9cf2a61a78
emmanuelsolis <emma.ruso@outlook.com>
2023-02-09 19:33:25 -0600

    Seccion headline and register for made and some change in the template desig

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,24 +1,31 @@
-import "./Intro.scss"
-export default function Intro(
-    
-) {
-  return (
-    <div className='intro' id="intro">
-      <div className="container">
-      <div className="left">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis amet, vitae maiores ea quibusdam cupiditate adipisci consectetur blanditiis numquam repellat qui in quia voluptatum fugiat tempore, cum iure unde nihil?</p>
-        <h1>Eslogan de la empresa</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui blanditiis ipsam, libero nam, quaerat minus quisquam non amet ad eius facere explicabo, deleniti incidunt quas temporibus dolorum necessitatibus. Facere, nobis.</p>
-      
+import "./Intro.scss";
+export default function Intro() {
 
-      
+  return (
+    <div className="intro" id="intro">
+     
+      <div className="container">
+        <div className="left">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            amet, vitae maiores ea quibusdam cupiditate adipisci consectetur
+            blanditiis numquam repellat qui in quia voluptatum fugiat tempore,
+            cum iure unde nihil?
+          </p>
+          <h1>Eslogan de la empresa</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+            blanditiis ipsam, libero nam, quaerat minus quisquam non amet ad
+            eius facere explicabo, deleniti incidunt quas temporibus dolorum
+            necessitatibus. Facere, nobis.
+          </p>
+        </div>
+        <div className="right">
+          <div className="imgContainer">
+            <img src="../../../public/assets/images/cam.jpg" alt="" />
+          </div>
+        </div>
       </div>
-      <div className="right">
-      <div className="imgContainer">
-          <img src="https://thumbs.dreamstime.com/b/persona-adolescente-sonriendo-y-saludando-caricatura-156730534.jpg" alt="" />
-        </div>
-        </div>
-        </div>
     </div>
-  )
+  );
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,6 +1,29 @@
-import "./Works.scss"
+import { FormLogin } from "../form/FormLogin";
+import "./Works.scss";
 export default function Works() {
   return (
-    <div className='works' id='works'>Works</div>
-  )
+    <div className="works" id="works">
+      <section className="top">
+        <div className="formulario">
+          <FormLogin />
+        </div>
+        <div className="headline">
+          <h4>Mapps</h4>
+          <h1>Headline</h1>
+          <h3>Subheading</h3>
+          <div className="pic">
+            <img
+              src="../../../public/assets/images/tecnicoprobando.jpg"
+              alt="Tecnico porbando camaras de seguridad"
+            />
+          </div>
+        </div>
+      </section>
+      <div className="buttomtext">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A delectus,
+        ratione et ullam at vero eos dicta temporibus culpa praesentium eum sint
+        in sed quae obcaecati dolorem omnis molestias possimus!
+      </div>
+    </div>
+  );
 }
