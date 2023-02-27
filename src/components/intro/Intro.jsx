398a839911cb71b28e79997f8aeedb9bd8462ccf
emmanuelsolis <emma.ruso@outlook.com>
2023-02-26 22:56:43 -0600

    efecto zoom en el banner de la portada

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,38 +1,35 @@
-import "./Intro.scss"
-import "./slider/Slider.scss"
-import Slider from "./slider/Slider"
-export default function Intro(
-    
-) {
+import "./Intro.scss";
+import "./slider/Slider.scss";
+import Slider from "./slider/Slider";
+export default function Intro() {
   return (
-    <div className='intro' id="intro">
-        
+    <div className="intro" id="intro">
+      <div className="box">
+        <div className="container">
+          <div className="left">
+            <h1>Eslogan de la empresa</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+              amet, vitae maiores ea quibusdam cupiditate adipisci consectetur
+              blanditiis numquam repellat qui in quia voluptatum fugiat tempore,
+              cum iure unde nihil?
+            </p>
 
-      <div className="container">
-        <div className="left">
-          <h1>Eslogan de la empresa</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            amet, vitae maiores ea quibusdam cupiditate adipisci consectetur
-            blanditiis numquam repellat qui in quia voluptatum fugiat tempore,
-            cum iure unde nihil?
-          </p>
-        
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
-            blanditiis ipsam, libero nam, quaerat minus quisquam non amet ad
-            eius facere explicabo, deleniti incidunt quas temporibus dolorum
-            necessitatibus. Facere, nobis.
-          </p>
-        </div>
-        {/* <div className="right">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui
+              blanditiis ipsam, libero nam, quaerat minus quisquam non amet ad
+              eius facere explicabo, deleniti incidunt quas temporibus dolorum
+              necessitatibus. Facere, nobis.
+            </p>
+          </div>
+          {/* <div className="right">
           <div className="imgContainer">
             <img src="../../../public/assets/images/cam.jpg" alt="" />
           </div>
         </div> */}
-
+        </div>
+      </div>
+      <Slider />
     </div>
-        <Slider/>
-  </div>
   );
 }
